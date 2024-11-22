f31e42e34fed8f71d0284400c8e5ab54a54ae535
Malte Boettcher <malte.boettcher@hypercode.de>
2024-11-22 16:12:06 +0100

    Show Toast on success

--- src/bookTime.tsx
+++ src/bookTime.tsx
@@ -1,4 +1,14 @@
-import { Action, ActionPanel, Form, LaunchProps, LocalStorage, showToast, Toast, useNavigation } from '@raycast/api'
+import {
+  Action,
+  ActionPanel,
+  Form,
+  LaunchProps,
+  LocalStorage,
+  showHUD,
+  showToast,
+  Toast,
+  useNavigation
+} from '@raycast/api'
 import { FormValidation, useForm, usePromise } from '@raycast/utils'
 import fetch from 'node-fetch'
 import { getProjects, getTasks, getTypesOfWork, task } from './composables/fetchData'
@@ -48,7 +58,9 @@ const bookTime = async (values: FormValues, tasks: task[] | undefined) => {
   }).catch((e: Error) => {
     showToast({ style: Toast.Style.Failure, title: e.name, message: e.message })
     console.log(e)
+    return
   })
+  await showHUD('Successfully booked time')
 }
 
 const validateDuration = (newDuration: string | undefined) => {
